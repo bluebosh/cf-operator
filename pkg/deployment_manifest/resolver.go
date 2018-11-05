a342ca9f9dab64ccc733966ec454b2a4e60efd5d
Mario Manno <mario.manno@suse.com>
2018-11-05 14:58:21 +0100

    fixup! fixup! spike testing

--- pkg/deployment_manifest/resolver.go
+++ pkg/deployment_manifest/resolver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	fissile "code.cloudfoundry.org/cf-operator/pkg/apis/fissile/v1alpha1"
+	"github.com/pkg/errors"
 	yaml "gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -20,14 +21,14 @@ func ResolveCRD(spec fissile.CFDeploymentSpec, client client.Client, namespace s
 	config := &corev1.ConfigMap{}
 	err := client.Get(context.TODO(), types.NamespacedName{Name: ref, Namespace: namespace}, config)
 	if err != nil {
-		return manifest, err
+		return manifest, errors.Wrap(err, "failed to retrieve via client.Get")
 	}
 
 	// unmarshal manifest.data into bosh deployment manifest...
 	// TODO re-use LoadManifest() from fisisle
 	m, ok := config.Data["manifest"]
 	if !ok {
-		return manifest, fmt.Errorf("ConfigMap doesn't contain manifest key")
+		return manifest, fmt.Errorf("configmap doesn't contain manifest key")
 	}
 	err = yaml.Unmarshal([]byte(m), manifest)
 
--- pkg/deployment_manifest/resolver_test.go
+++ pkg/deployment_manifest/resolver_test.go
@@ -7,17 +7,56 @@ import (
 	dm "code.cloudfoundry.org/cf-operator/pkg/deployment_manifest"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	fake "sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestResolveCRD(t *testing.T) {
 	assert := assert.New(t)
 
-	config := &corev1.ConfigMap{}
-	client := fake.NewFakeClient(config)
-	spec := fissile.CFDeploymentSpec{}
+	client := fake.NewFakeClient(
+		&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "default",
+			},
+			Data: map[string]string{"manifest": "---"},
+		},
+		&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "invalid_yaml",
+				Namespace: "default",
+			},
+			Data: map[string]string{"manifest": "!yaml"},
+		},
+		&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "missing_key",
+				Namespace: "default",
+			},
+			Data: map[string]string{},
+		},
+	)
+
+	spec := fissile.CFDeploymentSpec{ManifestRef: "foo"}
 	manifest, err := dm.ResolveCRD(spec, client, "default")
 
 	assert.NoError(err)
 	assert.NotNil(manifest)
+	assert.Equal(0, len(manifest.InstanceGroups))
+
+	spec = fissile.CFDeploymentSpec{ManifestRef: "bar"}
+	manifest, err = dm.ResolveCRD(spec, client, "default")
+	assert.Error(err)
+	assert.Contains(err.Error(), "configmaps \"bar\" not found")
+
+	spec = fissile.CFDeploymentSpec{ManifestRef: "missing_key"}
+	manifest, err = dm.ResolveCRD(spec, client, "default")
+	assert.Error(err)
+	assert.Contains(err.Error(), "configmap doesn't contain manifest key")
+
+	spec = fissile.CFDeploymentSpec{ManifestRef: "invalid_yaml"}
+	manifest, err = dm.ResolveCRD(spec, client, "default")
+	assert.Error(err)
+	assert.Contains(err.Error(), "yaml: unmarshal errors")
 }
